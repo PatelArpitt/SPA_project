d090e4ed1b453cbc020cc4057dc11a985f90cf42
PatelArpitt <patelarpit190@gmail.com>
2023-12-08 14:25:29 -0500

    Updated the HTML content as per word document and accessiblity is enhanced

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -11,12 +11,14 @@ function openTab(tab_name) {
     document.getElementById(tab_name).style.display = "block";
     document.getElementById(tab_name + 'Tab').classList.add("btn-warning")
     document.getElementById(tab_name + 'Tab').classList.add("active")
+
+    document.getElementById(tab_name).focus();
 }
 
 function openModal() {
     document.getElementById('myModal').style.display = 'block';
 }
-  
+
 function closeModal() {
     document.getElementById('myModal').style.display = 'none';
 }
@@ -24,10 +26,27 @@ function closeModal() {
 window.onclick = function (event) {
     var modal = document.getElementById('myModal');
     if (event.target == modal) {
-      modal.style.display = 'none';
+        modal.style.display = 'none';
     }
-}  
-  
+}
+
+function toggleSwitch() {
+    var switchElem = document.getElementById('toggleSwitch');
+    var textElem = document.getElementById('toggleText');
+
+    if (switchElem.checked) {
+        textElem.textContent = 'ON';
+        textElem.style.color = '#005603';
+    } else {
+        textElem.textContent = 'OFF';
+        textElem.style.color = '#CCC';
+    }
+}
+
+toggleSwitch();
+
+document.getElementById('toggleSwitch').addEventListener('change', toggleSwitch);
+
 function checkboxValidate(checked) {
     if (checked) document.getElementById("tell_us_more_div").style.display = "block"
     else document.getElementById("tell_us_more_div").style.display = "none"
@@ -37,8 +56,7 @@ function checkForm() {
     var email = document.schedule_call.email.value;
 
     if (!email) {
-        document.getElementById("formSubmitTrue").style.display = "none"
-        document.getElementById("formSubmitFalse").style.display = "block"
+        displayError('Email is required.');
         return false;
     }
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -55,6 +73,8 @@ function displayError(message) {
     document.getElementById("formSubmitTrue").style.display = "none";
     document.getElementById("formSubmitFalse").style.display = "block";
     document.getElementById("formSubmitFalse").innerText = message;
+
+    document.getElementById("formSubmitFalse").focus();
 }
 
 function hideError() {
